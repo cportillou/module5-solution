f82d8604ebd460703a908b8b1724afbb810591be
cportillou <141672009+cportillou@users.noreply.github.com>
2023-09-07 15:17:36 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,function(res){
-    var test = new Array (res.short_name);
+    var test = res.short_name;
     console.log(test);
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
